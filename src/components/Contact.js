50a71a41bab9aa378e5706e807c9825f9b4e6138
Villy Siu <villysiu@gmail.com>
2022-12-28 14:13:46 -0800

    remove spacing

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,12 +3,9 @@ const Contact =() => {
     const email="villysiu@gmail.com"
         return(
           <div className="contact"><p  >
-            2022 Zoo Logic created by Villy Siu</p>
-            <p > Have feedback, email me at 
+            2022 Zoo Logic created by Villy Siu<br/> Have feedback, email me at 
                 <Button variant="link" onClick={()=>window.open(`mailto:${email}`)}>{email}</Button>
-            </p>
-            
-            <p>
+            <br/>
             <a href='https://github.com/villysiu/zoo-logic-v3'>GitHub</a>
             {"   "}
             <a href='https://www.linkedin.com/in/villy-siu-384b81132/'>LinkedIn</a>
