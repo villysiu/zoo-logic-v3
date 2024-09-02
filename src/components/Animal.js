d06068d0126b2cf33e284a5c0227f2d534a83af0
Villy Siu <villysiu@gmail.com>
2024-09-02 00:11:04 -0700

    added not found for incorrect animalId

--- src/components/Animal.js
+++ src/components/Animal.js
@@ -1,18 +1,20 @@
 import lion from '../images/lion.png'
 import zebra from '../images/zebra.png'
 import hippo from '../images/hippo.png'
+import notFound from '../images/image-not-found-icon.png'
 import { memo } from 'react'
 const Animal = ({animalId}) => {
-    // console.log(animalId)
-    if(animalId===0) return null;
+
+    if(animalId<0 || animalId>3) 
+        return <img src={notFound} className="animal_icon" alt="" />
     
     const animalArray = [
-        [null, null], [ "hippo", hippo], ["lion", lion], ["zebra", zebra]
+        [], [ "hippo", hippo], ["lion", lion], ["zebra", zebra]
     ]
     
     const [animal, url] = animalArray[animalId];
     return (
-        <img src={url} alt={animal} />
+        <img src={url} className="animal_icon" alt={animal} />
     )
 }
 export default memo(Animal);
