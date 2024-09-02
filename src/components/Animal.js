2828fe5564a52768d605e7da36aacef4afea191c
Villy Siu <villysiu@gmail.com>
2024-09-02 00:04:31 -0700

    refactor from switch to array

--- src/components/Animal.js
+++ src/components/Animal.js
@@ -6,23 +6,13 @@ const Animal = ({animalId}) => {
     // console.log(animalId)
     if(animalId===0) return null;
     
-    const getAnimal = (animalId) =>{
-        switch (animalId) {
-            case 1:
-                return {id: 1, animal: "hippo", url: hippo}
-            case 2:
-                return {id: 2, animal: "lion", url: lion}
-            case 3:
-                return {id: 3, animal: "zebra", url: zebra}
-            default:
-                return {id: 0, animal: null, url: null}
-        }
-    }
-
-    const {animal, url} = getAnimal(animalId);
-
+    const animalArray = [
+        [null, null], [ "hippo", hippo], ["lion", lion], ["zebra", zebra]
+    ]
+    
+    const [animal, url] = animalArray[animalId];
     return (
-        <img src={url} width='50%' alt={animal}></img>
+        <img src={url} alt={animal} />
     )
 }
 export default memo(Animal);
